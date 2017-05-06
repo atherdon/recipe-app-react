d439da6d9cdb69f795f8008ca36680d0480e54fc
hirdbluebird <bluebirdyourdestiny@gmail.com>
2017-05-06 22:16:31 +0300

    removed react-native template

--- AppRouter.js
+++
@@ -1,8 +0,0 @@
-import { createRouter } from '@expo/ex-navigation';
-import HomeScreen from './HomeScreen';
-import DetailScreen from './DetailScreen';
-
-export default createRouter(() => ({
-  home: () => HomeScreen,
-  details: () => DetailScreen
-}));
